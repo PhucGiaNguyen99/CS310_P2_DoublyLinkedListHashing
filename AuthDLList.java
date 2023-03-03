8514510931646b1bbfb55c7534c49b704ec622ba
Phuc Nguyen <66092633+Saotalangim161099@users.noreply.github.com>
2023-03-03 03:44:15 -0500

    Complete work

--- AuthDLList.java
+++ AuthDLList.java
@@ -1,51 +1,76 @@
-
+/**
+ * Project 2: BLOCKED CHAIN AUTHENTICATED STORAGE
+ *
+ * Author: Phuc Nguyen
+ */
 public class AuthDLList {
+    /**
+     * Given start digest for the linked list.
+     */
     public static final String startDigest = "123456789";
+
+    /**
+     * Head of the linked list.
+     */
     public Node head;
+
+    /**
+     * Tail of the linked list.
+     */
     public Node tail;
 
     /**
-     * Check the authentication of the linked list by using proofCheck and the linked list from server.
+     * Check checks the authentication of the linked list by using the proofCheck and the linked list from server.
      *
-     * @param currentList current array list
-     * @param check       given proofCheck to compare with the last digest
-     * @return true if all 3 checks are passed. Otherwise, return false
-     * @throws IntegrityCheckFailedException
+     * @param currentList object of class AuthDLList
+     * @param check       given proofCheck
+     * @return true if all checks are passed. Otherwise, return false
+     * @throws IntegrityCheckFailedException if verifyIntegrity() returns false
      */
     public static boolean verifyIntegrity(AuthDLList currentList, String check) throws IntegrityCheckFailedException {
-        // starting from head
+        // traverse the linked list forwards from head
         Node currNode = currentList.head;
 
+        // if the linked list is empty, check if check is null. If yes, return true. Otherwise, return false
+        if (currNode == null) {
+            if (check != null) {
+                return false;
+            }
+        }
+
+        // boolean for first node of linked list
         boolean initial = true;
+
 
         while (currNode != null) {
             // if currentNode is head, then check whether its digest equals to startDigest+"&"+currentNode.file
             if (initial) {
 
-                // Check digest 1 = CHF (startDigest+"&"+file 1)
-                //String hash = Hashing.cryptHash(AuthDLList.startDigest + "&" + currNode.file);
-                String hash = (AuthDLList.startDigest + "&" + currNode.file);
-
+                // Check if the digest of head = hash(startDigest+"&"+file 1)
+                String hash = Hashing.cryptHash(AuthDLList.startDigest + "&" + currNode.file);
                 if (!currNode.digest.equals(hash)) {
-                    throw new IntegrityCheckFailedException();
+                    System.out.println("Return false in HEAD");
+                    return false;
                 }
-
-                // if it's matched, moved to remaining nodes
+                // if there's only one node in the linked list, check if the digest of head equals to check. Return false if it's not equal
+                if (currNode.next == null) {
+                    if (!currNode.digest.equals(check)) {
+                        System.out.println("Return false in case there is only one node");
+                        return false;
+                    }
+                }
                 initial = false;
             }
             // if currentNode is tail, check if its digest equals to proofCheck
             else if (currNode == currentList.tail) {
                 // if false, return false
-                if (!check.equals(currentList.tail.digest)) {
+                if (!(currNode.digest.equals(check))) {
                     return false;
                 }
-
             }
             // if currentNode is between head and tail, check whether its digest equals to the hash of its previous node's digest +"&"+currentNode.file
             else {
-                //String hash = Hashing.cryptHash(currNode.previous.digest + "&" + currNode.file);
-                String hash = (currNode.previous.digest + "&" + currNode.file);
-
+                String hash = Hashing.cryptHash(currNode.previous.digest + "&" + currNode.file);
                 if (!currNode.digest.equals(hash)) {
                     throw new IntegrityCheckFailedException();
                 }
@@ -56,179 +81,144 @@ public class AuthDLList {
     }
 
     /**
-     * Insert a node at the end of the Doubly linked list.
+     * Insert a node at the end of the linked list.
      *
      * @param data  data of the new node
-     * @param check proofCheck used for verifyIntegrity()
+     * @param check given proofCheck
      * @return the digest of the last node
-     * @throws IntegrityCheckFailedException
+     * @throws IntegrityCheckFailedException if verifyIntegrity() returns false
      */
     public String insertFileNode(String data, String check) throws IntegrityCheckFailedException {
-        //if (verifyIntegrity(head, check)) {
-        Node newNode = new Node();
-        newNode.file = data;
-        newNode.next = null;
+        if (verifyIntegrity(this, check)) {
+            Node newNode = new Node();
+            newNode.file = data;
+            newNode.next = null;
 
-        // if the linked list is empty, then make the new node as head
-        if (head == null) {
-            head = newNode;
-            // newNode.digest = Hashing.cryptHash(AuthDLList.startDigest + "&" + newNode.file);
-            newNode.digest = AuthDLList.startDigest + "&" + newNode.file;
-
+            // if the linked list is empty, then make the new node as head. And set its digest as for head
+            if (head == null) {
+                head = newNode;
+                newNode.digest = Hashing.cryptHash(AuthDLList.startDigest + "&" + newNode.file);
+            }
+            // otherwise, set next of tail to the new node, and set digest for new node
+            else {
+                tail.next = newNode;
+                newNode.digest = Hashing.cryptHash(tail.digest + "&" + newNode.file);
+            }
+            newNode.previous = tail;
+            tail = newNode;
+            return tail.digest;
         } else {
-            tail.next = newNode;
-            // newNode.digest = Hashing.cryptHash(tail.digest + "&" + newNode.file);
-            newNode.digest = (tail.digest + "&" + newNode.file);
-
+            throw new IntegrityCheckFailedException();
         }
-        newNode.previous = tail;
-        tail = newNode;
-        // already set file, previous, next and digest of newNode, which is our new tail
-        //}
-        return tail.digest;
     }
 
     /**
      * Delete the first node of the Doubly Linked list.
      *
      * @param check given proofCheck for verifyIntegrity()
-     * @return proofCheck of the linked list
-     * @throws IntegrityCheckFailedException
-     * @throws EmptyDLListException
+     * @return digest of the last node
+     * @throws IntegrityCheckFailedException if verifyIntegrity returns false
+     * @throws EmptyDLListException          if the linked list is empty
      */
     public String deleteFirstFile(String check) throws IntegrityCheckFailedException, EmptyDLListException {
-		/*
-			Develop Code here
-		*/
-        //if (verifyIntegrity(currentList, check)) {
-
-        // for regular doubly linked list
-        if (head != null) {
-            // save the current head
-            Node tempNode = head;
-
-            // if there is only one node. Now tail is empty, so Throw emptyDLinkedlistException......................................................................................................
-            if (head.next == null) {
-                tail = null;
-                head = null;
-                return null;
-            } else {
-                head.next.previous = null;
-                head = head.next;
-                head.previous = null;
-                // head.digest = Hashing.cryptHash(AuthDLList.startDigest + "&" + head.file);
-                head.digest = AuthDLList.startDigest + "&" + head.file;
-                // after changing digest of head, digests of all following nodes are changed, so we need to run while loop from node 2 to tail to update digests
-                Node currentNode = head.next;
-                while (currentNode != null) {
-                    currentNode.digest = currentNode.previous.digest + "&" + currentNode.file;
-                    currentNode=currentNode.next;
+        if (verifyIntegrity(this, check)) {
+            // if linked list is empty, throw EmptyDLListException()
+            if (head == null) {
+                throw new EmptyDLListException();
+            }
+            else {
+                // if there is only one node in the linked list. Set both head and tail to null and return null
+                if (head.next == null) {
+                    tail = null;
+                    head = null;
+                    return null;
                 }
+                // otherwise, delete the first node and set the new head with the digest for head. Then we need to modify digests of all following nodes
+                else {
+                    head.next.previous = null;
+                    head = head.next;
+                    head.previous = null;
+                    head.digest = Hashing.cryptHash(AuthDLList.startDigest + "&" + head.file);
+
+                    // run loop from second node to tail to update digests
+                    Node currentNode = head.next;
+                    while (currentNode != null) {
+                        currentNode.digest = Hashing.cryptHash(currentNode.previous.digest + "&" + currentNode.file);
+                        currentNode = currentNode.next;
+                    }
+                    return tail.digest;
+                }
+            }
+        }
+        // if the linked list is empty, throw emptyDLinkedListException
+        else {
+            throw new IntegrityCheckFailedException();
+        }
+    }
+
+    /**
+     * Delete the last node of the Linked list.
+     *
+     * @param check given proofCheck for verifyIntegrity()
+     * @return digest of the last node
+     * @throws IntegrityCheckFailedException if verifyIntegrity returns false
+     * @throws EmptyDLListException          if the linked list is empty
+     */
+    public String deleteLastFile(String check) throws IntegrityCheckFailedException, EmptyDLListException {
+        if (verifyIntegrity(this, check)) {
+            // if the linked list is empty, throw EmptyDLLException()
+            if (head == null) {
+                throw new EmptyDLListException();
+            }
+            // if there is only one node in the linked list, set both head and tail to null and return null
+            else if (head.next == null) {
+                head = null;
+                tail = null;
+                return null;
+            }
+            // otherwise, set tail to the previous node of tail
+            else {
+                tail = tail.previous;
+                tail.next = null;
+
+                // return new tail's digest as the proofCheck
                 return tail.digest;
             }
-        }
-        // if teh linked list is empty, throw emptyDLinkedListException
-        else {
-            throw new EmptyDLListException();
-        }
-
-        // }
-
-    }
-
-
-    public String deleteLastFile(String check) throws IntegrityCheckFailedException, EmptyDLListException {
-        //if (verifyIntegrity(currentList, check)) {
-        // if the linked list is empty
-        if (head == null) {
-            throw new EmptyDLListException();
-        }
-        // if there is only one node in the linked list
-        else if (head.next == null) {
-            head = null;
-            tail = null;
         } else {
-            tail = tail.previous;
-            tail.next = null;
-
-            // return new tail's digest as the proofCheck
-            return tail.digest;
+            throw new IntegrityCheckFailedException();
         }
-        //}
-        return null;
     }
 
 
-    // current is the given object of the Doubly linked list
-    // STATIC
-    // current is an object of the class AuthDList
+    /**
+     * Retrieve given node in the linked list.
+     *
+     * @param current object of class AuthDLList
+     * @param check   given proofCheck
+     * @param file    file of node to be retrieved
+     * @return node of given file
+     * @throws IntegrityCheckFailedException if verifyIntegrity() returns false
+     * @throws FileNotFoundException         if finally cannot find any node of given file
+     */
     public static Node retrieveNodeFile(AuthDLList current, String check, String file) throws
             IntegrityCheckFailedException, FileNotFoundException {
-
-        // searching from the head of the linked list
-        Node tempNode = current.head;
-        //if (verifyIntegrity(current, check)) {
-        if (current.head != null) {
-            while (tempNode != null) {
-                if (tempNode.file.equals(file)) {
-                    return tempNode;
+        if (verifyIntegrity(current, check)) {
+            // searching from the head of the linked list
+            Node tempNode = current.head;
+            if (current.head != null) {
+                while (tempNode != null) {
+                    if (tempNode.file.equals(file)) {
+                        return tempNode;
+                    }
+                    tempNode = tempNode.next;
                 }
-                tempNode = tempNode.next;
+                throw new FileNotFoundException();
+            } else {
+                return null;
             }
-            return null;
         } else {
-            throw new FileNotFoundException();
+            throw new IntegrityCheckFailedException();
         }
-        // }
-        //return null;
-    }
-
-    // Need to change it to private later
-    // Using for Testing
-    public String toStringBackwardFile() {
-        Node temp = tail;
-        String resultString = "";
-        while (temp != null) {
-            resultString += temp.file + " ";
-            temp = temp.previous;
-        }
-        return resultString;
-    }
-
-    // Need to change it to private later
-    // Using for Testing
-    public String toStringForwardFile() {
-        Node temp = head;
-        String resultString = "";
-        while (temp != null) {
-            resultString += temp.file + " ";
-            temp = temp.next;
-        }
-        return resultString;
-    }
-
-    // Need to change it to private later
-    // Using for Testing
-    public String toStringBackwardDigest() {
-        Node temp = tail;
-        String resultString = "";
-        while (temp != null) {
-            resultString += temp.digest + " ->";
-            temp = temp.previous;
-        }
-        return resultString;
-    }
-
-    // Need to change it to private later
-    // Using for Testing
-    public String toStringForwardDigest() {
-        Node temp = head;
-        String resultString = "";
-        while (temp != null) {
-            resultString += temp.digest + " ->";
-            temp = temp.next;
-        }
-        return resultString;
     }
 
 
--- AuthDLListTest.java
+++ AuthDLListTest.java
@@ -4,227 +4,134 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AuthDLListTest {
 
+    @Test
+    void verifyIntegrityWhenLinkedListIsEmpty() throws IntegrityCheckFailedException, EmptyDLListException {
+        AuthDLList authDLList = new AuthDLList();
+        authDLList.insertFileNode("Jan", null);
+        authDLList.insertFileNode("Feb", Hashing.cryptHash("123456789&Jan"));
+        authDLList.deleteLastFile(Hashing.cryptHash(Hashing.cryptHash("123456789&Jan") + "&" + "Feb"));
+        authDLList.deleteFirstFile(Hashing.cryptHash("123456789&Jan"));
+        assertTrue(AuthDLList.verifyIntegrity(authDLList, null));
+    }
+
     @org.junit.jupiter.api.Test
     void verifyIntegrity() throws IntegrityCheckFailedException, EmptyDLListException {
         AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.insertFileNode("May", "Something");
-        authDLList.deleteLastFile("Something");
-        //authDLList.deleteLastFile("Something");
-        authDLList.insertFileNode("Jun", "Something");
-        authDLList.insertFileNode("Jul", "Something");
-        //authDLList.deleteFirstFile("Something");
-        System.out.println("Tail's digest: " + authDLList.tail.digest);
-        assertTrue(AuthDLList.verifyIntegrity(authDLList, "123456789&Feb&Apr&Jun&Jul"));
+        authDLList.insertFileNode("Jan", null);
+        authDLList.insertFileNode("Feb", Hashing.cryptHash("123456789&Jan"));
+        authDLList.deleteLastFile(Hashing.cryptHash(Hashing.cryptHash("123456789&Jan") + "&" + "Feb"));
+        authDLList.deleteFirstFile(Hashing.cryptHash("123456789&Jan"));
+        authDLList.insertFileNode("Mar", null);
+        authDLList.insertFileNode("Apr", Hashing.cryptHash("123456789&Mar"));
+        assertTrue(AuthDLList.verifyIntegrity(authDLList, Hashing.cryptHash(Hashing.cryptHash("123456789&Mar") + "&Apr")));
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyIntegrityDeleteOneNode() throws IntegrityCheckFailedException, EmptyDLListException {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyIntegrityDeleteEmptyList() throws IntegrityCheckFailedException, EmptyDLListException {
+
     }
 
     @Test
     void verifyIntegrityInsertDeleteFirst() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        assertTrue(AuthDLList.verifyIntegrity(authDLList, ""));
+
     }
 
     @Test
     void verifyIntegrityInsertDeleteLast() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        //authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteLastFile("Something");
-        assertTrue(AuthDLList.verifyIntegrity(authDLList, "123456789&Feb"));
+
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteFirstFileForwardOneNode() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.deleteFirstFile("Something");
-        assertEquals("", authDLList.toStringBackwardFile());
+
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteFirstFileBackwardOneNode() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.deleteFirstFile("Something");
-        assertEquals("", authDLList.toStringBackwardFile());
+
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteFirstFileForwardNodes() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        assertEquals("", authDLList.toStringForwardFile());
+
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteFirstFileBackwardNodes() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        assertEquals("Apr ", authDLList.toStringBackwardFile());
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteLastFileForwardOneNode() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.deleteLastFile("Something");
-        assertEquals("", authDLList.toStringForwardFile());
+
     }
 
     @org.junit.jupiter.api.Test
     void deleteLastFileBackwardOneNode() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.deleteLastFile("Something");
-        assertEquals("", authDLList.toStringBackwardFile());
+
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteLastFileForwardNodes() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        assertEquals("", authDLList.toStringForwardFile());
+
     }
 
     // PASSED
     @org.junit.jupiter.api.Test
     void deleteLastFileBackwardNodes() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        assertEquals("Jan ", authDLList.toStringBackwardFile());
+
     }
 
 
     @org.junit.jupiter.api.Test
     void retrieveNodeFile() throws IntegrityCheckFailedException, FileNotFoundException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.insertFileNode("May", "Something");
-        Node foundNode = AuthDLList.retrieveNodeFile(authDLList, "Something", "Jan");
-        assertEquals("Jan", foundNode.file);
+
+
     }
 
 
     // PASSED
     @Test
     void insertFileNodeForFileForward() throws IntegrityCheckFailedException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.insertFileNode("May", "Something");
-        assertEquals("Jan Feb Mar Apr May ", authDLList.toStringForwardFile());
+
     }
 
     // PASSED
     @Test
     // Test backward of the linked list
     void insertFileNodeForFileBackward() throws IntegrityCheckFailedException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.insertFileNode("May", "Something");
-        assertEquals("May Apr Mar Feb Jan ", authDLList.toStringBackwardFile());
+
     }
 
     @Test
     void insertFileNodeForDigestForward() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        System.out.println("Tail: " + authDLList.tail.digest);
 
-        //authDLList.deleteLastFile("Something");
-        //authDLList.insertFileNode("May", "Something");
-        assertEquals("123456789&Jan ->", authDLList.toStringForwardDigest());
     }
 
     // PASSED
     @Test
     // Test backward of the linked list
-    void insertFileNodeForDigestBackward() throws IntegrityCheckFailedException, EmptyDLListException {
+    void insertFileNodeForDigestBackward() throws IntegrityCheckFailedException, EmptyDLListException, FileNotFoundException {
         AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.insertFileNode("May", "Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteFirstFile("Something");
-        System.out.println("Tail: " + authDLList.tail.digest);
-        assertEquals("Apr&May ->123456789&Apr ->", authDLList.toStringBackwardDigest());
+        authDLList.insertFileNode("Jan", null);
+        authDLList.insertFileNode("Feb", Hashing.cryptHash("123456789&Jan"));
+        authDLList.insertFileNode("Mar", Hashing.cryptHash(Hashing.cryptHash("123456789&Jan") + "&Feb"));
+        authDLList.insertFileNode("Apr", Hashing.cryptHash(Hashing.cryptHash(Hashing.cryptHash("123456789&Jan") + "&Feb") + "&Mar"));
+        assertEquals("Apr", AuthDLList.retrieveNodeFile(authDLList, Hashing.cryptHash(Hashing.cryptHash(Hashing.cryptHash(Hashing.cryptHash("123456789&Jan") + "&Feb") + "&Mar") + "&Apr"), "Ka").file);
     }
 
     @Test
     void testGeneralExceptVerifyIntegrity() throws IntegrityCheckFailedException, EmptyDLListException {
-        AuthDLList authDLList = new AuthDLList();
-        authDLList.insertFileNode("Jan", "Something");
-        authDLList.insertFileNode("Feb", "Something");
-        authDLList.insertFileNode("Mar", "Something");
-        authDLList.deleteFirstFile("Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.insertFileNode("Apr", "Something");
-        authDLList.insertFileNode("May", "Something");
-        authDLList.deleteLastFile("Something");
-        authDLList.deleteLastFile("Something");
-        System.out.println("Tail: " + authDLList.tail.digest);
-        assertEquals("123456789&Feb ->", authDLList.toStringForwardDigest());
+
     }
 }
